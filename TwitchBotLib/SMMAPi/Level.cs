9d75b159dbb38bbc930b4fbe2a3e5e4cf76abbbf
dram55 <isbister1500@hotmail.com>
2016-01-27 15:05:55 -0500

    Parsing API with Newtonsoft.Json

---
+++ TwitchBotLib/SMMAPi/Level.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+
+namespace TwitchBotLib.SMMAPi
+{
+    /// <summary>
+    /// Public facing API object for a SMM level
+    /// </summary>
+    public class Level
+    {
+        public string Attempts { get; }
+        public decimal ClearRate { get; }
+        public string Author { get; }
+        public string ThumbnailURL { get; }
+        public string Title { get; }
+
+        public Level(string json)
+        {
+            try
+            {
+                JToken response = JObject.Parse(json);
+                Attempts = (string)response.SelectToken("attempts");
+                ClearRate = (decimal)response.SelectToken("clear_rate");
+                Author = (string)(response.SelectToken("creator").SelectToken("display_name"));
+                ThumbnailURL = (string)response.SelectToken("thumbnail");
+                Title = (string)response.SelectToken("title");
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException();
+            }
+        }
+
+    }
+}
--- TwitchBotLib/SMMAPi/SMMApi.cs
+++ TwitchBotLib/SMMAPi/SMMApi.cs
@@ -1,78 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace TwitchBotLib.SMMAPi
 {
 
-    interface APIParser
-    {
-        Queue<string> APIParseTree { get; }
-        string APIDirectory { get;}
-    }
-
-    class LevelParser : APIParser
-    {
-        public string APIDirectory
-        {
-            get
-            {
-                return "course/";
-            }
-        }
-
-        private Queue<string> apiParseTree;
-        public Queue<string> APIParseTree
-        {
-            get
-            {
-                if (apiParseTree == null)
-                {
-                    apiParseTree = new Queue<string>();
-                    apiParseTree.Enqueue("attempts");
-                    apiParseTree.Enqueue("clear_rate");
-                    apiParseTree.Enqueue("display_name");
-                    apiParseTree.Enqueue("thumbnail");
-                    apiParseTree.Enqueue("title");
-                }
-                return new Queue<string>(apiParseTree);
-            }
-        }
-    }
-
-    public class Level
-    {
-        public string Attempts { get;  }
-        public string ClearRate { get; }
-        public string Author { get; }
-        public string ThumbnailURL { get; }
-        public string Title { get; }
-
-        public Level(Queue<string> properties)
-        {
-            Attempts = properties.Dequeue();
-            ClearRate = properties.Dequeue();
-            Author = properties.Dequeue();
-            ThumbnailURL = properties.Dequeue();
-            Title = properties.Dequeue();
-        }
-
-    }
-
-
 
     public class SMMApi
     {
+        const string MARIO_API_SITE = "http://smm.butthole.tv/";
+
         public static Level GetLevel(string code)
         {
-            Queue<string> levelProperties = ParseAPI(new LevelParser(), code);
-            if (levelProperties != null)
+            string returnJSON = APIRequest("course/", code);
+            if (returnJSON != "")
             {
-                Level tempLevel = new Level(levelProperties);
+                Level tempLevel = new Level(returnJSON);
                 return tempLevel;
             }
             else
@@ -81,56 +25,33 @@ namespace TwitchBotLib.SMMAPi
             }
         }
 
-        private static Queue<string> ParseAPI(APIParser APIToParse, string query)
+        private static string APIRequest(string directory, string query)
         {
-            Queue<string> returnTree = new Queue<string>();
-
+            string json = "";
+            
             try
             {
-                string requestUrl = "http://smm.butthole.tv/"+ APIToParse.APIDirectory + query;
+                string requestUrl = MARIO_API_SITE + directory + query;
                 WebClient client = new WebClient();
                 Stream stream = client.OpenRead(requestUrl);
+                
                 using (StreamReader reader = new StreamReader(stream))
                 {
-
-                    Queue<string> parseTree = APIToParse.APIParseTree;
-
-
-                    while (!reader.EndOfStream && parseTree.Count > 0 )
-                    {
-                        string temp = reader.ReadLine();
-                        string format_tag = "\"" + parseTree.Peek() + "\":";
-                        if (temp.Contains(format_tag))
-                        {
-                            string nodeValue = temp.Replace(format_tag, "").Replace("\"", "").Trim().TrimEnd(',');
-                            returnTree.Enqueue(nodeValue);
-                            parseTree.Dequeue();
-                            //Console.WriteLine(nodeValue);
-                        }
-                    }
-
-                    if (parseTree.Count > 0 )
-                    {
-                        //Console.WriteLine("Return JSON unable to be parsed completely. Did not find all attributes.");
-                        return null;
-                    }
-
+                    json = reader.ReadToEnd();
                 }
             }
             catch(WebException e)
             {
                 //Console.WriteLine("Return Invavlid Level"); 404 not found
-                return null;
+                return "";
             }
             catch (Exception ex)
             {
                 //Console.WriteLine("Invalid Request");
-                return null;
+                return "";
             }
 
-            return returnTree;
-            
-
+            return json;
         }
     }
 }
